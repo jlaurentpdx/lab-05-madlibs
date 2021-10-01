88a4836fddecbecfd3659b63ee45f200d9775388
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-01 16:19:24 -0700

    add HTML and JS for 2nd amendment and CSS for header and inputs

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const noun2Span = document.getElementById('noun-2-span');
 const noun3Span = document.getElementById('noun-3-span');
 const verb1Span = document.getElementById('verb-1-span');
 
+//Second Amendment Input
+const noun4Input = document.getElementById('noun-4-input');
+const noun5Input = document.getElementById('noun-5-input');
+const noun6Input = document.getElementById('noun-6-input');
+const verb2Input = document.getElementById('verb-2-input');
+
+//Second Amendment Span
+const noun4Span = document.getElementById('noun-4-span');
+const noun5Span = document.getElementById('noun-5-span');
+const noun6Span = document.getElementById('noun-6-span');
+const verb2Span = document.getElementById('verb-2-span');
+
 submitButton.addEventListener('click', () => {
     amendments.classList.remove('hidden');
 
